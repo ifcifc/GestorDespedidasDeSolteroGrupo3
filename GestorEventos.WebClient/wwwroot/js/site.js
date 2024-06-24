3fe4e3f68bbe6a9577c668b25f85460c3fab3ef6
ifcifc <168789366+ifcifc@users.noreply.github.com>
2024-06-23 21:20:14 -0300

    Cambios en la pagina de inicio

--- GestorEventos.WebClient/wwwroot/js/site.js
+++ GestorEventos.WebClient/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(() => {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    $('#tooltipSesion').tooltip('show');
+    $('#tooltipSesion').on('hidden.bs.tooltip', () => {
+        $('#tooltipSesion').tooltip('disable');
+    });
+    setTimeout(() => {
+        $('#tooltipSesion').tooltip('hide');
+    }, 1300);
+});
