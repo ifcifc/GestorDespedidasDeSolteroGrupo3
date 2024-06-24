8d05f8d52947ab927e7238205375ff5fcd21a91b
ifcifc <168789366+ifcifc@users.noreply.github.com>
2024-06-24 01:38:29 -0300

    Arreglando lios de una Collie salvaje. Ahora solo se puede ver cambios si inician sesion, debido a alguien hace cambios sin ver la pagina completa. El proximo que meta mano en FixMain.css le corto la mano.

--- GestorEventos.WebClient/Controllers/HomeController.cs
+++ GestorEventos.WebClient/Controllers/HomeController.cs
@@ -1,19 +1,23 @@
 using GestorEventos.Servicios.Controller;
+using GestorEventos.Servicios.Controllers;
+using GestorEventos.Servicios.Entidades;
+using GestorEventos.Servicios.Servicios;
 using GestorEventos.WebClient.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace GestorEventos.WebClient.Controllers
 {
+    [Authorize]
     public class HomeController : SimpleController
     {
-
-        public IActionResult Privacy()
+        public override ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Reserva()
+        public IActionResult Privacy()
         {
             return View();
         }
